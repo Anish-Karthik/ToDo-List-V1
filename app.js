cf4f38de597e9fe2a6557392ece20ca4f8404598
Anish-Karthik <anishkarthik.54321@gmail.com>
2023-04-08 20:48:34 +0530

    Add Dynamic form to add list and Change color for dayType

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var taskList = ['Buy Food', 'Cook Food', 'Eat Food'];
+
 app.get('/', (req, res) => {
     var today = new Date();
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var dayType = today.getDay()==6 || today.getDay()==0 ? 'weekend' : 'weekday';
     var date = today.toLocaleDateString('en-US', options);
+    res.render('list', {date: date, dayType: dayType, taskList: taskList});
+});
 
-    res.render('list', {date: date});
+app.post('/', (req, res) => {
+    var newTask = req.body.newTask;
+    taskList.push(newTask);
+    res.redirect('/');
 });
 
 app.listen(3000, () => {
