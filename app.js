18c76e4e22926838f46060a66abfcb7e4f1ee7e8
Anish-Karthik <anishkarthik.54321@gmail.com>
2023-04-09 11:14:56 +0530

    navigate using route variable

--- app.js
+++ app.js
@@ -6,20 +6,31 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var workList = ['Complete the course', 'Finish the project', 'Ace the exam',];
 var taskList = ['Buy Food', 'Cook Food', 'Eat Food'];
 
 app.get('/', (req, res) => {
     var today = new Date();
     var options = { weekday: 'long', month: 'long', day: 'numeric' };
-    var dayType = today.getDay()==6 || today.getDay()==0 ? 'weekend' : 'weekday';
     var date = today.toLocaleDateString('en-US', options);
-    res.render('list', {date: date, dayType: dayType, taskList: taskList});
+
+    res.render('list', {ListTitle: date, taskList: taskList, route: '/'});
 });
 
 app.post('/', (req, res) => {
     var newTask = req.body.newTask;
     taskList.push(newTask);
     res.redirect('/');
+});
+
+app.get('/work', (req, res) => {
+    res.render('list', {ListTitle: 'Work List', taskList: workList, route: '/work'});
+});
+
+app.post('/work', (req, res) => {
+    var newTask = req.body.newTask;
+    workList.push(newTask);
+    res.redirect('/work');
 });
 
 app.listen(3000, () => {
