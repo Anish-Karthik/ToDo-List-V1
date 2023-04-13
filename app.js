376d5146365d5943a02d95f4c629bd342694fe7a
Anish-Karthik <anishkarthik.54321@gmail.com>
2023-04-13 22:56:42 +0530

    Add port

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/work', (req, res) => {
     workList.push(newTask);
     res.redirect('/work');
 });
-
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log('Server started on port 3000');
 });
